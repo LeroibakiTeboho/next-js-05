f67bef9795afabbe38c0d6b07e023ad0bd2307b3
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-07-03 18:28:24 +0200

    opting out of caching

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,29 +1,40 @@
-import React from 'react'
+export const fetchCache = "default-cache";
+
+import React from "react";
 
 type Product = {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-}
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+};
 
 async function ProductsPage() {
-    const response = await fetch("http://localhost:3001/products");
-    const products = await response.json();
+
+
+    const detailsResponse = await fetch("http://localhost:3001/products/1");
+      const details = await detailsResponse.json();
+
+
+  const response = await fetch("http://localhost:3001/products", {
+    cache: 'no-store'
+  });
+  const products = await response.json();
   return (
-    <ul className='space-y-4 p-4'>
-         {products.map((product: Product) => (
-            <li 
-                key={product.id}
-                className='p-4 bg-white shadow-md rounded-lg text-gray-700'>
-            <h2 className='text-xl font-semibold'>{product.title}</h2>
-            <p>{product.description}</p>
-            <p className='text-lg font-medium'>${product.price}</p>
-            
-            </li>
-         ))}
+    <ul className="space-y-4 p-4">
+      {products.map((product: Product) => (
+        <li
+          key={product.id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          <h2 className="text-xl font-semibold">{product.title}</h2>
+          <p>{product.description}</p>
+          <p className="text-lg font-medium">${product.price}</p>
+          <p className="text-red-500">{details.price}</p>
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
 
-export default ProductsPage
+export default ProductsPage;
