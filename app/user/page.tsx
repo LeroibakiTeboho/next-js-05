c9e425490f119857af6ec056c06d6677879797b4
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-07-03 17:37:13 +0200

    loading and error states

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -15,24 +15,23 @@ async function UsersPage() {
 
   return (
     <div className="grid grid-cols-2 gap-2 p-4">
-        {users.map((user: User) => (
-            <div 
-            key={user.id}
-            className="flex items-center justify-between p-4 bg-white shadow rounded-lg text-gray-700"
-            >
-                <div className="flex flex-col space-y-1">
-                    <h2 className="text-lg font-semibold">{user.name}</h2>
-                    <p className="text-md">{user.username}</p>
-                </div>
-                <div className="flex flex-col space-y-1 items-end">
-                    <div className="text-md">{user.email}</div>
-                    <div className="text-md">{user.phone}</div>
-                </div>
-            </div>
-        ))}
+      {users.map((user: User) => (
+        <div
+          key={user.id}
+          className="flex items-center justify-between p-4 bg-white shadow rounded-lg text-gray-700"
+        >
+          <div className="flex flex-col space-y-1">
+            <h2 className="text-lg font-semibold">{user.name}</h2>
+            <p className="text-md">{user.username}</p>
+          </div>
+          <div className="flex flex-col space-y-1 items-end">
+            <div className="text-md">{user.email}</div>
+            <div className="text-md">{user.phone}</div>
+          </div>
+        </div>
+      ))}
     </div>
-
-)
+  );
 }
 
 export default UsersPage;
